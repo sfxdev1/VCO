098fbacef1f627e8a4a59802de9c0f9870114c76
sfxdev <sfx.coding@gmail.com>
2016-11-25 09:57:18 -0600

    Working on /round_data

---
+++ src/main/java/com/keith/vco/RoundInfo.java
@@ -0,0 +1,14 @@
+package com.keith.vco;
+
+/**
+ * Created by Development on 11/25/2016.
+ */
+public class RoundInfo {
+    public int id;
+    public int players;
+
+    public RoundInfo(int id, int players) {
+        this.id = id;
+        this.players = players;
+    }
+}
